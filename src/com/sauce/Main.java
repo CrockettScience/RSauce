79d60ba23ca02f22e3fe3763382cb123ef299276
CrockettScience <johncrockett15@gmail.com>
2017-08-16 09:56:51 -0700

    0.0.2 Integrated project information object.

--- src/com/sauce/Main.java
+++ src/com/sauce/Main.java
@@ -13,13 +13,21 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
-public class LWJGLHelloWorld {
+/**
+ * Created by John Crockett.
+ */
+
+public class Main {
 
     // The window handle
     private long window;
+    private boolean running = true;
+    // The game loop handle
+    public static Main LOOP = new Main();
 
     private void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+        System.out.println("Hello RSauce " + Project.VERSION + "!");
 
         init();
         loop();
@@ -36,7 +44,7 @@ public class LWJGLHelloWorld {
     private void init() {
         // Setup an error callback. The default implementation
         // will print the error message in System.err.
-        GLFWErrorCallback.createPrint(System.err).set();
+        GLFWErrorCallback.createPrint(System.out).set();
 
         // Initialize GLFW. Most GLFW functions will not work before doing this.
         if ( !glfwInit() )
@@ -45,10 +53,11 @@ public class LWJGLHelloWorld {
         // Configure GLFW
         glfwDefaultWindowHints(); // optional, the current window hints are already the default
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will be resizable
 
         // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        window = glfwCreateWindow(300, 300, Project.NAME, NULL, NULL);
+
         if ( window == NULL )
             throw new RuntimeException("Failed to create the GLFW window");
 
@@ -95,11 +104,11 @@ public class LWJGLHelloWorld {
         GL.createCapabilities();
 
         // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while ( !glfwWindowShouldClose(window) ) {
+        while ( !glfwWindowShouldClose(window) && running) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
             glfwSwapBuffers(window); // swap the color buffers
@@ -110,8 +119,12 @@ public class LWJGLHelloWorld {
         }
     }
 
+    public void quitAtEndOfCycle(){
+        running = false;
+    }
+
     public static void main(String[] args) {
-        new LWJGLHelloWorld().run();
+        LOOP.run();
     }
 
 }
---
+++ src/com/sauce/Project.java
@@ -0,0 +1,10 @@
+package com.sauce;
+
+/**
+ * Created by John Crockett.
+ */
+public class Project {
+    public static String NAME = "Sauce";
+    public static String VERSION = "0.0.0";
+
+}
