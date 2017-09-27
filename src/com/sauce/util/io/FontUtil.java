b2696d53adc769567127b92761ae02720e0b9c46
Jonathan Crockett <johncrockett15@gmail.com>
2017-09-26 21:34:43 -0700

    0.3.4 Dev 2 - FontInfo and IOFont classes created

--- src/com/sauce/util/io/FontUtil.java
+++ src/com/sauce/util/io/FontUtil.java
@@ -1,4 +1,20 @@
 package com.sauce.util.io;
 
+import java.nio.ByteBuffer;
+
 public class FontUtil {
+
+    public class FontInfo{
+
+    }
+
+    public class IOFont extends ResourceUtil.IOResource {
+
+        private FontInfo fontInfo;
+
+        private IOFont(ByteBuffer aBuffer, FontInfo info) {
+            super(aBuffer);
+            fontInfo = info;
+        }
+    }
 }
