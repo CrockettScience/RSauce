687e2c5a57d05341a68a422750955ca6b45a9ffa
Jonathan Crockett <johncrockett15@gmail.com>
2017-10-09 15:25:08 -0700

    0.3.6 Dev 3 - Created TextureRegion class

--- src/com/sauce/asset/graphics/TextureAtlas.java
+++ src/com/sauce/asset/graphics/TextureAtlas.java
@@ -1,4 +1,22 @@
 package com.sauce.asset.graphics;
 
+import com.util.Vector2DDouble;
+
 public class TextureAtlas {
+
+    public class TextureRegion {
+        private Vector2DDouble p00;
+        private Vector2DDouble p01;
+        private Vector2DDouble p10;
+        private Vector2DDouble p11;
+        private int texPageID;
+
+        private TextureRegion(double x, double y, double width, double height, int texID){
+            p00 = new Vector2DDouble(x, y);
+            p01 = new Vector2DDouble(x, y + height);
+            p10 = new Vector2DDouble(x + width, y);
+            p11 = new Vector2DDouble(x + width, y + height);
+            texPageID = texID;
+        }
+    }
 }
